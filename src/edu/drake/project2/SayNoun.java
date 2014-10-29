9b611b05ef35d97297e11dffa6aa22ea28a3ddb0
jvandervaart <jacob.vandervaart@drake.edu>
2014-10-28 21:33:06 -0500

    added the timer

--- src/edu/drake/project2/SayNoun.java
+++ src/edu/drake/project2/SayNoun.java
@@ -3,7 +3,6 @@ package edu.drake.project2;
 
 
 import java.io.IOException;
-import java.util.TimerTask;
 import java.util.Vector;
 
 import android.content.Context;
@@ -11,6 +10,7 @@ import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -24,25 +24,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class SayNoun extends ActionBarActivity {
-	
+
 	private static final String LOG_TAG = "Audio Record";
 	private String outputFile0, outputFile1, outputFile2, outputFile3, outputFile4, storyOutputFile, outputFileName = null;
 	private MediaRecorder myAudioRecorder;
-	private boolean recording = false;
 	Vector <String> outputFileNames = new Vector <String>();
 	int i=0, numPrompts=0;
 	Story story;
 	TextView txtLabel;
-
 	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_say_noun);
-		// hide the action bar
-        getActionBar().hide();
-        
+
 		int category = getIntent().getIntExtra("category", -1);
 
 		//check to see if category was passed correctly
@@ -59,39 +55,31 @@ public class SayNoun extends ActionBarActivity {
 		txtLabel.setText(story.myPromptList.get(i).getPromptString());
 
 		setUpAudio();
-		
+
 		//set up timer and mic button
 		ImageButton micBtn = (ImageButton) findViewById(R.id.imageButton1);
 
 		micBtn.setOnClickListener( new OnClickListener() {
 
-		
+
 			@Override
 			public void onClick(View arg0) {
-				
-				//currently you need to click the button once to start recording
-				//then again to stop recording. We should implement the timer later
-				if (!recording){
-					startRecording();
-				}
-				if (recording){
-					stopRecording();
-				}
-				recording = !recording;
 
-			    //start timer. 
-				//new Timer().schedule(stop(), 3000);
-				//message("button worked"); 
+				startRecording();
+				//stops recording after 3 seconds and call the next method.
+				try {
+					new Handler().postDelayed(new Runnable() {
+						@Override
+						public void run() {
+							stopRecording();
+							next();
+						}
+					}, 3000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-
 		});
-	}
-
-	private TimerTask stop() {
-		myAudioRecorder.stop();
-	    myAudioRecorder.release();
-	    myAudioRecorder  = null;
-		return null;
 	}
 
 	public void startRecording(){
@@ -113,16 +101,15 @@ public class SayNoun extends ActionBarActivity {
 		myAudioRecorder.start(); 
 		message("Started Recording"); 
 	}
-	
+
 	//stops the recording
 	public void stopRecording(){
 		myAudioRecorder.stop();
-	    myAudioRecorder.release();
-	    myAudioRecorder  = null;
+		myAudioRecorder.release();
+		myAudioRecorder  = null;
 		//display message to confirm recording has stopped
-		message("Stopped Recording");
 	}
-		
+
 	//method for showing message to user
 	public void message(String message){
 		Context context = getApplicationContext();
@@ -131,7 +118,7 @@ public class SayNoun extends ActionBarActivity {
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
 	}
-	
+
 	public void setUpAudio(){
 		//create the 5 hard-coded output file names. 
 		Log.i("Audio", "inside the audio start method");
@@ -140,9 +127,9 @@ public class SayNoun extends ActionBarActivity {
 		outputFile2 = Environment.getExternalStorageDirectory().getAbsolutePath() + "/res2.3gp";;
 		outputFile3 = Environment.getExternalStorageDirectory().getAbsolutePath() + "/res3.3gp";;
 		outputFile4 = Environment.getExternalStorageDirectory().getAbsolutePath() + "/res4.3gp";;
-		
+
 		outputFileName = outputFile0;
-		
+
 		Log.i("Audio", "Created the output files");
 		outputFileNames.add(outputFile0);
 		outputFileNames.add(outputFile1);
@@ -150,38 +137,37 @@ public class SayNoun extends ActionBarActivity {
 		outputFileNames.add(outputFile3);
 		outputFileNames.add(outputFile4);
 		Log.i("Audio", "Added the outputFiles to the vector");
-		
+
 	}
 
-	public void next(View view){
+	public void next(){
 		ImageView image = (ImageView) findViewById(R.id.progress);
-		int progress[] = {
-				getResources().getIdentifier("drawable/progress_button_2", null, getPackageName()),
-				getResources().getIdentifier("drawable/progress_button_3", null, getPackageName()),
-				getResources().getIdentifier("drawable/progress_button_4", null, getPackageName()),
-				getResources().getIdentifier("drawable/progress_button_5", null, getPackageName())
-				};
+		int progress[] = {getResources().getIdentifier("drawable/dog3", null, getPackageName()),
+				getResources().getIdentifier("drawable/dog4", null, getPackageName()),
+				getResources().getIdentifier("drawable/dog5", null, getPackageName()),
+				getResources().getIdentifier("drawable/dog6", null, getPackageName())};
+
 		if(i<4){
 			i++;
 			txtLabel.setText(story.myPromptList.get(i).getPromptString());
 			outputFileName = outputFileNames.elementAt(i);
 			image.setImageResource(progress[i-1]);
-			String promptFileName = story.commandFileNames.get(i);
-			//wait for x time, then play the command stored in the promptFileName
-			
 		}
 		else{
 			Log.i("next", "in the else statment");
-			//Problem starts here.
-			//Intent intent = new Intent(getApplicationContext(), Play.class);
-			//intent.putExtra("story", story);
-			//Problem ends here.
-			Intent intent = new Intent(this, Play.class);
+			//create the storyOutputFile
+			//storyOutputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/story.3gp";;
+
+			//concatenate the strings here
+
+			//go to the play my story screen
+			Intent intent = new Intent(getApplicationContext(), Play.class);
+
+			//intent.putExtra("storyPath", storyOutputFile);
 			startActivity(intent);
-			
 		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
